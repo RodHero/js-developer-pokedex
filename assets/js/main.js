4487d2436d9f7d75107b0439b14ee8073473f231
RodHero <miroli.rod.1@gmail.com>
2022-12-08 17:46:17 -0300

    Versão funcional dos detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton');
-const nome = document.getElementById('staticBackdropLabel');
-const tipo = document.getElementById('tipo')
-const imagem = document.getElementById('imagem')
+
 
 
 const maxRecords = 151
@@ -37,17 +35,80 @@ async function fetchPokemon(numero) {
 }
 
 async function renderPoke(numero) {
+    const nome = document.getElementById('staticBackdropLabel');
+    const tipo1 = document.getElementById('tipo1')
+    const tipo2 = document.getElementById('tipo2')
+    const imagem = document.getElementById('imagem')
+    const peso = document.getElementById('peso')
+    const altura = document.getElementById('altura')
+    const back = document.getElementById('back')
+    const cabeça = document.getElementById('cabeça')
+    const rodape = document.getElementById('rodape')
+    const fechar = document.getElementById('fechar')
+    const hp = document.getElementById('hp')
+    const att = document.getElementById('att')
+    const def = document.getElementById('def')
+    const satt = document.getElementById('satt')
+    const sdef = document.getElementById('sdef')
+    const spd = document.getElementById('spd')
+    const mov1 = document.getElementById('mov1')
+    const mov2 = document.getElementById('mov2')
+
+
+
     const dados = await fetchPokemon(numero) || {};
     if (dados) {
         nome.innerHTML = "#"+dados.id+" "+dados.name;
-        tipo.innerHTML = dados['type'];
-        imagem.innerHTML = dados['sprites'];
+        tipo1.innerHTML = dados['types']['0']['type']['name'];
+        try {
+            tipo2.innerHTML = dados['types']['1']['type']['name'];
+            tipo2.classList.toggle(dados['types']['1']['type']['name'])
+        }
+        catch (err){
+            tipo2.innerHTML = ""
+
+        }
+        imagem.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        imagem.alt = dados.name;
+        peso.innerHTML ="Weight: " + dados.weight + "lb.";
+        altura.innerHTML ="Height: " +  dados.height + "ft.";
+        hp.innerHTML = "HP:   "+dados['stats']['0']['base_stat'];
+        att.innerHTML = "Attack:  "+dados['stats']['1']['base_stat'];
+        def.innerHTML = "Defense:  "+dados['stats']['2']['base_stat'];
+        satt.innerHTML = "Sp.Attack:  "+dados['stats']['3']['base_stat'];
+        sdef.innerHTML = "Sp.Defense:  "+dados['stats']['4']['base_stat'];
+        spd.innerHTML = "Speed:  "+dados['stats']['5']['base_stat'];
+        
+
+        
+        back.classList.toggle(dados['types']['0']['type']['name'])
+        cabeça.classList.toggle(dados['types']['0']['type']['name'])
+        rodape.classList.toggle(dados['types']['0']['type']['name'])
+        tipo1.classList.toggle(dados['types']['0']['type']['name'])
+        peso.classList.toggle(dados['types']['0']['type']['name'])
+        altura.classList.toggle(dados['types']['0']['type']['name'])
+        mov1.classList.toggle(dados['types']['0']['type']['name'])
+        mov2.classList.toggle(dados['types']['0']['type']['name'])
 
         
     }
     else {
         nome.innerHTML = 'Not found :c';
     }
+
+    fechar.addEventListener('click',() => {
+        back.className = 'modal-body'
+        cabeça.className = 'modal-header'
+        rodape.className = 'modal-footer'
+        tipo1.classList = ''
+        tipo2.classList = ''
+        peso.classList = ''
+        altura.classList = ''
+        mov1.classList = 'moves1'
+        mov2.classList = 'moves2'
+
+
+    })
 }
 
 function loadPokemonItens(offset, limit) {
